8423dbed929a43cdbf0264f6bf8c0bebf8f68705
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2023-04-17 21:19:21 -0300

    add: global styles in project

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -7,11 +7,20 @@ export const {
   globalCss,
   keyframes,
   theme,
-  createTheme
+  createTheme,
+  getCssText
 } = createStitches({
   theme: {
     colors: {
-      rocketseat: '#8257e6'
+      white: '#FFF',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e',
     }
   }
 })
