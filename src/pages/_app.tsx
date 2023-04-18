88d71e800905d28190caee338cd523d209bb43fc
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2023-04-17 21:31:46 -0300

    add: header in the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
 import type { AppProps } from 'next/app'
 
 import { globalStyles } from '../styles/global'
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <header>
-        <img src="" alt="" />
-      </header>
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Ignite Shop" />
+      </Header>
       <Component {...pageProps} />
-    </div>
+    </Container>
   )
 }
