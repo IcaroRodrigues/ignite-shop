dcc0b73f27edc3b5a0fad54b7918c43b2d7c6c4a
IcaroRodrigues <Kevin_Lothar@hotmail.com>
2023-05-02 19:06:53 -0300

    add: success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import { ImageContainer, SuccessContainer } from '@/styles/pages/success';
+import Link from 'next/link';
+
 export default function Success() {
-  return(
-    <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Icaro Rodrigues</strong>, sua <strong>Camiseta Beyound the Limits </strong>
+        já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
